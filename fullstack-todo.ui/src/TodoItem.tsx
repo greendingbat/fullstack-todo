5a1d2effdd3e556e89bd4af4e6298b5d4d90ad97
greendingbat <matthew-burns@live.com>
2025-06-09 19:13:53 -0600

    ui adjustments, debounce in place

--- fullstack-todo.ui/src/TodoItem.tsx
+++ fullstack-todo.ui/src/TodoItem.tsx
@@ -1,5 +1,17 @@
-import { useState } from "react";
-import { Button, Checkbox, Icon } from "semantic-ui-react";
+import { useCallback, useRef, useState } from "react";
+import { Button, Checkbox, Icon, type CheckboxProps } from "semantic-ui-react";
+
+const debounce = (callback: Function, delay: number) => {
+  let timeoutId: number | undefined = undefined;
+
+  return (...args: any[]) => {
+    window.clearTimeout(timeoutId);
+
+    timeoutId = window.setTimeout(() => {
+      callback.apply(null, args);
+    }, delay);
+  };
+};
 
 interface ITodoItemProps {
   title: string;
@@ -8,15 +20,30 @@ interface ITodoItemProps {
 }
 
 const TodoItem = ({ title, description, completed }: ITodoItemProps) => {
-  const [itemTitle, setItemTitle] = useState(title);
-  const [itemDescription, setItemDescription] = useState(description);
-  const [checked, setChecked] = useState(completed);
-  const [editing, setEditing] = useState(false);
+  const [itemTitle, setItemTitle] = useState<string>(title);
+  const [itemDescription, setItemDescription] = useState<string>(description);
+  const [checked, setChecked] = useState<boolean>(completed);
+  const [editing, setEditing] = useState<boolean>(false);
 
-  const handleCheckboxChange = () => {
-    setChecked(!checked);
-    // send debounced request to update item
-  };
+  const debouncedLogRef = useRef(
+    debounce(() => console.log("sending update request"), 500)
+  );
+
+  const handleCheckboxChange = useCallback(
+    (e: React.FormEvent<HTMLInputElement>, data: CheckboxProps) => {
+      setChecked(data.checked as boolean);
+      debouncedLogRef.current();
+    },
+    []
+  );
+
+  const handleEditButtonClick = useCallback(() => {
+    if (editing) {
+      // Save changes
+      debouncedLogRef.current();
+    }
+    setEditing(!editing);
+  }, [editing]);
 
   const titleComponent = editing ? (
     <input
@@ -65,12 +92,17 @@ const TodoItem = ({ title, description, completed }: ITodoItemProps) => {
     <div
       style={{
         border: "1px solid #ccc",
-        backgroundColor: "#f9f9f9",
+        backgroundColor: checked ? "#d9d9d9" : "#fff",
         margin: "4px 0",
         padding: "4px",
       }}
     >
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
         <div>
           <div
             style={{
@@ -96,8 +128,9 @@ const TodoItem = ({ title, description, completed }: ITodoItemProps) => {
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
+              border: "none",
             }}
-            onClick={() => setEditing(!editing)}
+            onClick={handleEditButtonClick}
           >
             {editing ? (
               "Save"
