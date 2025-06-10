a7443ddffce08daf4414ce1f942e18aa5a9a8b6a
greendingbat <matthew-burns@live.com>
2025-06-09 19:46:12 -0600

    remaining CRUD endpoints implemented

--- fullstack-todo.api/src/index.ts
+++ fullstack-todo.api/src/index.ts
@@ -19,6 +19,75 @@ app.get("/api/todoItems", async (req, res) => {
   res.json(todoItems);
 });
 
+app.post("/api/todoItems", async (req, res) => {
+  const { itemTitle, itemDesc, completed = false } = req.body;
+
+  // itemTitle and itemDesc are required fields
+  if (!itemTitle || !itemDesc) {
+    res.status(400).json({ error: "itemTitle and itemDesc are required" });
+  }
+
+  try {
+    const todoItem = await prisma.todoItem.create({
+      data: {
+        itemTitle,
+        itemDesc,
+        completed,
+      },
+    });
+    res.json(todoItem);
+  } catch (error) {
+    res.status(500).send("Error creating todo item");
+  }
+});
+
+app.put("/api/todoItems/:id", async (req, res) => {
+  const { id } = req.params;
+  const { itemTitle, itemDesc, completed } = req.body;
+
+  // for updating, itemTitle, itemDesc, and completed are required
+  if (!itemTitle || !itemDesc || completed === undefined) {
+    res
+      .status(400)
+      .json({ error: "itemTitle, itemDesc, and completed are required" });
+  }
+
+  if (isNaN(Number(id))) {
+    res.status(400).json({ error: "ID must be a number" });
+  }
+
+  try {
+    const todoItem = await prisma.todoItem.update({
+      where: { id: Number(id) },
+      data: {
+        itemTitle,
+        itemDesc,
+        completed,
+      },
+    });
+    res.json(todoItem);
+  } catch (error) {
+    res.status(500).send(`Error updating todo item with ID ${id}`);
+  }
+});
+
+app.delete("/api/todoItems/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (isNaN(Number(id))) {
+    res.status(400).json({ error: "ID must be a number" });
+  }
+
+  try {
+    await prisma.todoItem.delete({
+      where: { id: Number(id) },
+    });
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).send(`Error deleting todo item with ID ${id}`);
+  }
+});
+
 // #endregion
 
 app.listen(SERVER_PORT, () => {
